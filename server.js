288d3a13c3966135b5cb09a266fe56b3ce853441
naufalkps <naufalkps77@gmail.com>
2020-09-04 12:18:13 +0700

    membuat controller.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 //parse application/json
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(3000, () => {
-    console.log(`Server started on port`);
+    console.log("Server started on port");
 });
