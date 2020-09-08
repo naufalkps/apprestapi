b2b7aa14a0453e40d4659c74d0b9fa3a1e68be82
naufalthufail <naufalkps77@gmail.com>
2020-09-08 07:44:02 +0700

    membuat menu tambah data

--- controller.js
+++ controller.js
@@ -9,11 +9,11 @@ exports.index = function (req, res) {
 
 //menampilkan semua data mahasiswa
 exports.tampilsemuamahasiswa = function (req, res) {
-    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
         if (error) {
             console.log(error);
         } else {
-            response.ok(rows, res)
+            response.ok(rows, res);
         }
     });
 };
@@ -26,7 +26,23 @@ exports.tampilberdasarkanid = function (req, res) {
             if (error) {
                 console.log(error);
             } else {
-                response.ok(rows, res);
+                response.ok(rows, res)
+            }
+        });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES (?,?,?)', [nim, nama, jurusan],
+        function (error, rows, field) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Behasil Menambahkan Data!", res)
             }
         });
 };
